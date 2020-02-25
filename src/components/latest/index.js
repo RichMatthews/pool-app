08208e615fa23766d57d69effd8a418369922eff
Kieran Mountford <kieran@paddle.com>
2020-02-25 17:34:18 +0000

    Update index page

--- src/components/latest/index.js
+++ src/components/latest/index.js
@@ -11,12 +11,12 @@ export const Latest = ({ matches, players }) => {
                     const winnerImage = findUserImage(match.winner, players)
                     const loserImage = findUserImage(match.loser, players)
                     return (
-                        <p>
-                            <img src={winnerImage} />
+                        <p style={{ textTransform: 'uppercase', fontSize: '0.8rem', marginTop: '0.3rem' }}>
+                            <img src={winnerImage} style={{ verticalAlign: 'middle', height: '2rem', width: '2rem', marginRight: '0.5rem'}}/>
                             <span style={{ color: 'green' }}>{match.winner}</span>
-                            <span style={{ color: '#fff' }}> vs </span>
+                            <span style={{ color: '#fff', margin: '0.5rem 1rem'}}> vs </span>
                             <span style={{ color: 'red' }}>{match.loser}</span>
-                            <img src={loserImage} />
+                            <img src={loserImage} style={{ verticalAlign: 'middle', height: '2rem', width: '2rem', marginLeft: '0.5rem'}}/>
                         </p>
                     )
                 })}
