6c0409ae890252adcb8bcb2749acc378beb98c1b
Rich Matthews <richmatthews@btinternet.com>
2020-03-21 19:23:01 +0000

    fix more stuff

--- src/components/latest/index.js
+++ src/components/latest/index.js
@@ -1,23 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { findUserImage } from 'utils/findUserImage'
+
+const Game = styled.div`
+    align-items: center;
+    color: #fff;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 15px;
+    width: 150px;
+`
 
 export const Latest = ({ matches, players }) => {
     const slicedMatches = [...matches].reverse()
     return (
         <div>
             {slicedMatches &&
-                slicedMatches.slice(0, 3).map(match => {
+                slicedMatches.slice(0, 5).map(match => {
                     const winnerImage = findUserImage(match.winner, players)
                     const loserImage = findUserImage(match.loser, players)
                     return (
-                        <p style={{ textTransform: 'uppercase', fontSize: '0.8rem', marginTop: '0.3' }}>
-                            <img src={winnerImage} style={{ verticalAlign: 'middle', height: '2rem', width: '2rem', marginRight: '0.5rem'}}/>
-                            <span style={{ color: 'green' }}>{match.winner}</span>
-                            <span style={{ color: '#fff', margin: '0.5rem 1rem'}}> vs </span>
-                            <span style={{ color: 'red' }}>{match.loser}</span>
-                            <img src={loserImage} style={{ verticalAlign: 'middle', height: '2rem', width: '2rem', marginLeft: '0.5rem'}}/>
-                        </p>
+                        <Game>
+                            <img
+                                src={winnerImage}
+                                style={{
+                                    verticalAlign: 'middle',
+                                    height: '2rem',
+                                    width: '2rem',
+                                    marginRight: '0.5rem',
+                                }}
+                            />
+                            beat
+                            <img
+                                src={loserImage}
+                                style={{ verticalAlign: 'middle', height: '2rem', width: '2rem', marginLeft: '0.5rem' }}
+                            />
+                        </Game>
                     )
                 })}
         </div>
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -13,20 +13,53 @@ const Container = styled.div`
     margin-left: ${({ menuHidden }) => (menuHidden ? '-410px' : 0)};
 `
 
+const LinkContainer = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    padding-left: 5px;
+`
 const StyledLink = styled(Link)`
-    margin-bottom: 20px;
-    color: #787878;
-    text-decoration: none;
+    align-items: center;
     border-bottom: 1px solid #787878;
+    color: #787878;
+    display: flex;
+    margin-bottom: 20px;
+    text-decoration: none;
+    width: 100%;
+`
+const Image = styled.img`
+    height: 30px;
+    width: 30px;
 `
 
 export const Nav = ({ toggleMenu, menuHidden }) => {
     return (
         <Container onClick={() => toggleMenu(!menuHidden)} menuHidden={menuHidden}>
-            <StyledLink to="">Home</StyledLink>
-            <StyledLink to="league-table">League Table</StyledLink>
-            <StyledLink to="record">Record match</StyledLink>
-            <StyledLink to="seasons">Past Seasons</StyledLink>
+            <LinkContainer>
+                <StyledLink to="">
+                    <Image src={'/images/home.svg'} />
+                    <p style={{ marginLeft: '10px' }}>Home</p>
+                </StyledLink>
+            </LinkContainer>
+            <LinkContainer>
+                <StyledLink to="league-table">
+                    <Image src={'/images/record.svg'} />
+                    <p style={{ marginLeft: '10px' }}>League Table</p>
+                </StyledLink>
+            </LinkContainer>
+            <LinkContainer>
+                <StyledLink to="record">
+                    <Image src={'/images/record.svg'} />
+                    <p style={{ marginLeft: '10px' }}>Record Match</p>
+                </StyledLink>
+            </LinkContainer>
+            <LinkContainer>
+                <StyledLink to="seasons">
+                    <Image src={'/images/past.svg'} />
+                    <p style={{ marginLeft: '10px' }}>Past Seasons</p>
+                </StyledLink>
+            </LinkContainer>
         </Container>
     )
 }
